48804209316efefc784a8a5a9914f5b84e1cb1d0
Kaushik Bokka <kaushikbokka@gmail.com>
2021-02-25 12:51:23 +0530

    fix imports

--- pytorch_lightning/plugins/training_type/smddp.py
+++ pytorch_lightning/plugins/training_type/smddp.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
 from pytorch_lightning import _logger as log
 from pytorch_lightning.overrides import LightningDistributedModule
 from pytorch_lightning.plugins.training_type.ddp import DDPPlugin
