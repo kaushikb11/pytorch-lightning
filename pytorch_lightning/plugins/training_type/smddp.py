8ee599dc896ceb01e157410c7e758c24aa4f8986
Kaushik Bokka <kaushikbokka@gmail.com>
2021-02-26 17:00:29 +0530

    add pre dispatch to smddp

--- pytorch_lightning/plugins/training_type/smddp.py
+++ pytorch_lightning/plugins/training_type/smddp.py
@@ -13,10 +13,13 @@
 # limitations under the License.
 import os
 
+import torch
+
 from pytorch_lightning import _logger as log
 from pytorch_lightning.overrides import LightningDistributedModule
 from pytorch_lightning.plugins.training_type.ddp import DDPPlugin
 from pytorch_lightning.utilities import _SMDIST_AVAILABLE
+from pytorch_lightning.utilities.seed import seed_everything
 
 if _SMDIST_AVAILABLE:
     import smdistributed.dataparallel.torch.distributed as dist
@@ -24,6 +27,50 @@ if _SMDIST_AVAILABLE:
 
 
 class SMDDPPlugin(DDPPlugin):
+
+    distributed_backend = "smddp"
+
+    def pre_dispatch(self):
+        # TODO: check if needed
+        seed = os.environ.get("PL_GLOBAL_SEED")
+        if seed is not None:
+            seed_everything(int(seed))
+
+        # determine which process we are and world size
+        self.set_world_ranks()
+
+        # set warning rank
+        rank_zero_only.rank = self.global_rank
+
+        # set up server using proc 0's ip address
+        # try to init for 20 times at max in case ports are taken
+        # where to store ip_table
+        self.init_ddp_connection(self.global_rank, self.world_size)
+
+        # TODO: we moved it to the trainer.fit after calling pre_dispatch
+        #   ... need to double check that it is the correct place
+        # self.trainer.call_setup_hook(self.model)
+
+        # on world_size=0 let everyone know training is starting
+        if self.is_global_zero and not dist.is_initialized():
+            log.info("-" * 100)
+            log.info(f"distributed_backend={self.distributed_backend}")
+            log.info(f"All DDP processes registered. Starting ddp with {self.world_size} processes")
+            log.info("-" * 100)
+
+        # set the ranks and devices
+        self.dist.rank = self.global_rank
+        self.dist.device = self.root_device
+
+        if self.sync_batchnorm:
+            self.model = self.configure_sync_batchnorm(self.model)
+
+        # move the model to the correct device
+        self.model_to_device()
+
+        self.configure_ddp()
+
+        self.barrier()
 
     def init_ddp_connection(self, global_rank: int, world_size: int) -> None:
 
